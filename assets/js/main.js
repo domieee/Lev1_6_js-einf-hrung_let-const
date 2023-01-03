d225edba124cc2336919b16acd4a8e61489b0bbb
domieee <99103235+domieee@users.noreply.github.com>
2023-01-03 17:17:09 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/* Aufgabenstellung
+
+In dieser Übung werden wir den Unterschied zwischen let und const lernen.
+
+
+Deklariere die Variable let x mit dem Wert 34.
+Überschreibe x anschließend mit dem Wert 67.
+Gib den Wert x in der Konsole aus.
+Danach kommentiere deinen Code aus.
+
+
+/* let x = 34;
+x = 67;
+console.log(x); */
+
+/* Deklariere die Variable const y mit dem Wert 34.
+Überschreibe y anschließend mit dem Wert 67.
+Gib den Wert y in der Konsole aus.
+Danach kommentiere deinen Code aus. */
+
+/* const y = 34;
+y = 67;
+console.log(y); */
+
+/* Was ist der Unterschied? */
+
+    /* Daten vom Typ 'const' (abl. Konstante) geben in der Regel Summen an die ihren Wert nicht verändern. Ein Beispiel wäre zum Beispiel die Zahl Pi. Probiert man den Wert zu ändern, wird ein Error angezeigt.
+    Daten vom Typ 'let' hingegen können überschrieben werden.*/
+
